0e1f6c931088b0ddcfebcd1ad1b8e7855f399a36
BuildTools <unconfigured@null.spigotmc.org>
2015-10-12 15:45:01 +1100

    Base folder and class setup

---
+++ com/qualcomm/ftcrobotcontroller/Auto_test.java
@@ -0,0 +1,15 @@
+package com.qualcomm.ftcrobotcontroller;
+
+import com.qualcomm.ftcrobotcontroller.CustomBase;
+
+public class Auto_test extends CustomBase {
+	@Override
+	public void startup() {
+		
+	}
+	
+	@Override
+	public void loop() {
+		
+	}
+}
---
+++ com/qualcomm/ftcrobotcontroller/CustomBase.java
@@ -0,0 +1,14 @@
+package com.qualcomm.ftcrobotcontroller;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class Auto_test extends OpMode {
+	@Override
+	public void init() {
+		
+	}
+	
+	public void startup() {
+		
+	}
+}
