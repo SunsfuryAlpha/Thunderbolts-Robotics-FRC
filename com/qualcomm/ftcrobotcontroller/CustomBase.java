54df27f8a4341cf784e7a768c93821d2d6cbc7dc
YoshiRulz <yoshiisawsm@gmail.com>
2015-10-13 16:58:36 +1100

    Base code

--- com/qualcomm/ftcrobotcontroller/CustomBase.java
+++ com/qualcomm/ftcrobotcontroller/CustomBase.java
@@ -3,12 +3,27 @@ package com.qualcomm.ftcrobotcontroller;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 public class Auto_test extends OpMode {
+	DcMotor leftTank = hardwareMap.dcMotor.get("left_drive");
+	DcMotor rightTank = hardwareMap.dcMotor.get("right_drive");
+	
+	public boolean driveDirection = 1;
+	
 	@Override
 	public void init() {
-		
+		startup();
 	}
 	
 	public void startup() {
 		
+	}
+	
+	public void reverseDrive() {
+		if (driveDirection > 0) {
+			rightTank.setDirection(DcMotor.Direction.REVERSE); //TODO: check value in DcMotor.Direction
+			leftTank.setDirection(DcMotor.Direction.FORWARD); //TODO: check name
+		} else {
+			rightTank.setDirection(DcMotor.Direction.FORWARD);
+			leftTank.setDirection(DcMotor.Direction.REVERSE);
+		}
 	}
 }
