3885e0ca4e3076de2b1c7dddba7e2055fa5e4563
YoshiRulz <yoshiisawsm@gmail.com>
2015-10-16 17:00:45 +1100

    Updated references and added input processing scaffold

--- com/qualcomm/ftcrobotcontroller/Teleop_test.java
+++ com/qualcomm/ftcrobotcontroller/Teleop_test.java
@@ -1,8 +1,13 @@
 package com.qualcomm.ftcrobotcontroller;
 
-import com.qualcomm.ftcrobotcontroller.CustomBase;
+import com.qualcomm.ftcrobotcontroller.CustomDrivebase;
 
-public class Auto_test extends CustomBase {
+public class Auto_test extends CustomDrivebase {
+	@Override
+	public static final String driveType = "tank";
+	
+	public static final int[] CONTROLMAP = {};
+	
 	@Override
 	public void startup() {
 		
@@ -10,6 +15,30 @@ public class Auto_test extends CustomBase {
 	
 	@Override
 	public void loop() {
-		
+		if (/* controller lstick 'KEYMAP[forward]' up */){
+			if (/* controller rstick 'KEYMAP[forward]' up */) {
+				forward(max(/* lstick, rstick */));
+			} else if (/* controller rstick 'KEYMAP[forward]' down */) {
+				turnRight(min(/* lstick, -rstick */));
+			} else {
+				turnRight(/* lstick */);
+			}
+		} else if (/* controller lstick 'KEYMAP[forward]' down */) {
+			if (/* controller rstick 'KEYMAP[forward]' up */) {
+				turnLeft(min(/* -lstick, rstick */));
+			} else if (/* controller rstick 'KEYMAP[forward]' down */) {
+				backward(max(/* lstick, rstick */));
+			} else {
+				turnLeft(/* -lstick */);
+			}
+		} else {
+			if (/* rstick > 0 */) {
+				turnLeft(/* rstick */);
+			} else if () {
+				turnRight(/* -rstick */);
+			} else {
+				stop();
+			}
+		}
 	}
 }
