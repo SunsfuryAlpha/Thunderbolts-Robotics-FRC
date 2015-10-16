ed3881234050477bd9388c6934394d7bb88b8e74
YoshiRulz <yoshiisawsm@gmail.com>
2015-10-16 17:00:15 +1100

    Updated references

--- com/qualcomm/ftcrobotcontroller/Auto_test.java
+++ com/qualcomm/ftcrobotcontroller/Auto_test.java
@@ -1,8 +1,13 @@
 package com.qualcomm.ftcrobotcontroller;
 
-import com.qualcomm.ftcrobotcontroller.CustomBase;
+import com.qualcomm.ftcrobotcontroller.CustomDrivebase;
 
-public class Auto_test extends CustomBase {
+public class Auto_test extends CustomDrivebase {
+	@Override
+	public static final String driveType = "tank";
+	
+	public static final int[] CONTROLMAP = {};
+	
 	@Override
 	public void startup() {
 		
